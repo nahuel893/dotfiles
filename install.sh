479c4f830b2c2bddf211a31c8fd397c73c69d0e0
nahuel893 <nahuel893@hotmail.com>
2026-02-18 15:40:44 +0000

    Add AstroNvim symlinks to install.sh

--- install.sh
+++ install.sh
@@ -22,6 +22,16 @@ link .config/hypr/hyprland.conf
 link .config/hypr/hyprpaper.conf
 link .config/starship.toml
 link .config/fontconfig/fonts.conf
+link .config/nvim/lua/community.lua
+link .config/nvim/lua/lazy_setup.lua
+link .config/nvim/lua/polish.lua
+link .config/nvim/lua/plugins/astrocore.lua
+link .config/nvim/lua/plugins/astrolsp.lua
+link .config/nvim/lua/plugins/astroui.lua
+link .config/nvim/lua/plugins/mason.lua
+link .config/nvim/lua/plugins/none-ls.lua
+link .config/nvim/lua/plugins/treesitter.lua
+link .config/nvim/lua/plugins/user.lua
 link .config/rofi/config.rasi
 link .config/waybar/config.jsonc
 link .config/waybar/style.css
