00cc847b8e9940ece3020270cbd8b3a5272c9f45
nahuel893 <nahuel893@hotmail.com>
2026-02-18 15:40:36 +0000

    Add transparent background to AstroNvim (polish.lua)

---
+++ .config/nvim/lua/polish.lua
@@ -0,0 +1,8 @@
+-- Transparent background (lets kitty blur show through)
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE" })
